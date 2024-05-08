e17c70e9c8be1b6127e3464027348677a0e5d711
Denis16543 <158287693+Denis16543@users.noreply.github.com>
2024-05-08 14:49:40 +0300

    закінчення 169 сторінки

--- market.js
+++ market.js
@@ -8,25 +8,36 @@ let itemsDiv = document.getElementById("items");
 //Перевірка існування знайденого блоку
 if (itemsDiv) {
     //Вивід знайденого елементу
-    console.log(itemsDiv)
+    //console.log(itemsDiv)
     //Вивід значення поля знайденого елементу
-    console.log('Поле classList: ', itemsDiv.classList)
-    console.log('Поле id: ', itemsDiv.id)
-    console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+    //console.log('Поле classList: ', itemsDiv.classList)
+   // console.log('Поле id: ', itemsDiv.id)
+   // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+   // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
 
     //Додавання тексту в блок
-    itemsDiv.innerText = 'Перший Програмно доданий текст'
-    itemsDiv.innerText += 'Другий Програмно доданий текст'
+    //itemsDiv.innerText = 'Перший Програмно доданий текст'
+    //itemsDiv.innerText += 'Другий Програмно доданий текст'
     //Додавання відформатованого HTML коду в блок
     //itemsDiv.innerHTML += '<div class = "item"></div>'
    // itemsDiv.innerHTML += '<div class = "item"></div>'
    // itemsDiv.innerHTML += '<div class = "item"></div>'
    // itemsDiv.innerHTML += '<div class = "item"></div>'
-   for (let i = 0; i< 100; i++) {
-    itemsDiv.innerHTML += '<div class = item></div>'
-   }
-} else {
+   //for (let i = 0; i< 100; i++) {
+    //itemsDiv.innerHTML += '<div class = item></div>'
+   //}
+
+} 
+else {
     //Вивід повідомлення про не знайдений блок 
     console.log('Блок товарів не знайдено')
+}
+//Визначення масиву товарів
+let itemsArray = ['','','','']
+
+//Виведення в консоль масиву
+console.log(itemsArray)
+//Виведення в консоль елементів масиву
+for (let i = 0; i < itemsArray.length; i++){
+    console.log(itemsArray[i])
 }
